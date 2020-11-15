b453164f0dbf90743e7c6cd93b117e4d36ea3400
kabbala <kabbala@users.noreply.github.com>
2020-11-15 14:42:39 +0900

    replace inheritage support code
    These changes are mailed to given email address of author, however, DokuWiki Developer didn't get an answer yet.

--- action.php
+++ action.php
@@ -17,25 +17,76 @@ class action_plugin_headerfooter extends DokuWiki_Action_Plugin {
     }
     public function handle_parser_wikitext_preprocess(Doku_Event &$event, $param) {
         global $INFO;
+        global $ID;
+        global $conf;
+        
+        //what does this mean???
         if ($INFO['id'] != '') return; // 发现每页会执行两次，当id为空时是真正的文本，否则是菜单。
-        $inf = pageinfo();
-        $inf['namespace'] = urlencode(str_replace(array(' ', '%', '&'), '_', $inf['namespace']));
-        $ns = str_replace(':', '/', $inf['namespace']) . '/';
-        $base = str_replace('\\', '/', DOKU_INC) . 'data/pages/' . $ns; // 得到文件绝对路径
-        file_put_contents('tt.txt','abc');
-        if (file_exists($base . '_header.txt')){ // 存在头文件
-            $header = file_get_contents($base . '_header.txt');
-            if ($this->getConf('separation') == 'paragraph'){ // 如果使用段落来分割
-                $header = rtrim($header, " \r\n\\") . "\n\n";
+
+        //helper array needed for parsePageTemplate
+        //so that replacement like shown here is possible: https://www.dokuwiki.org/namespace_templates#replacement_patterns
+        $data = array(
+            'id'        => $ID, // the id of the page to be created
+            'tpl'       => '', // the text used as template
+        );
+
+        $headerpath = '';
+        $path = dirname(wikiFN($ID));
+        if (@file_exists($path.'/_header.txt')) {
+            $headerpath = $path.'/_header.txt';
+        } else {
+            // search upper namespaces for templates
+            $len = strlen(rtrim($conf['datadir'], '/'));
+            while (strlen($path) >= $len) {
+                if (@file_exists($path.'/__header.txt')) {
+                    $headerpath = $path.'/__header.txt';
+                    break;
+                }
+                $path = substr($path, 0, strrpos($path, '/'));
             }
-            $event->data = $header . $event->data;
         }
-        if (file_exists($base . '_footer.txt')){ // 存在尾文件
-            $footer = file_get_contents($base . '_footer.txt');
-            if ($this->getConf('separation') == 'paragraph'){ // 如果使用段落来分割
-                $footer = "\n\n" . ltrim($footer, " \r\n\\");
+
+        if (!empty($headerpath)) {
+            $header = file_get_contents($headerpath);
+            if ($header !== false) {
+                $data['tpl'] = cleanText($header);
+                $header = parsePageTemplate($data);
+
+                if ($this->getConf('separation') == 'paragraph') { // 如果使用段落来分割
+                    $header = rtrim($header, " \r\n\\") . "\n\n";
+                }
+                $event->data = $header . $event->data;
             }
-            $event->data .= $footer;
+        }
+
+
+        $footerpath = '';
+        $path = dirname(wikiFN($ID));
+        if (@file_exists($path.'/_footer.txt')) {
+            $footerpath = $path.'/_footer.txt';
+        } else {
+            // search upper namespaces for templates
+            $len = strlen(rtrim($conf['datadir'], '/'));
+            while (strlen($path) >= $len) {
+                if (@file_exists($path.'/__footer.txt')) {
+                    $footerpath = $path.'/__footer.txt';
+                    break;
+                }
+                $path = substr($path, 0, strrpos($path, '/'));
+            }
+        }
+
+        if (!empty($footerpath)) {
+            $footer = file_get_contents($footerpath);
+            if ($footer !== false) {
+                $data['tpl'] = cleanText($footer);
+                $footer = parsePageTemplate($data);
+
+                if ($this->getConf('separation') == 'paragraph') { // 如果使用段落来分割
+                    $footer = rtrim($footer, " \r\n\\") . "\n\n";
+                }
+                $event->data .= $footer;
+            }
         }
     }
 }
